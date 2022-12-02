5bc4c8c2dfcfca31d9aad8eef2c92e8565bf3e74
Neal Erickson <neal@erickson.io>
2022-12-02 09:08:06 -0500

    --wip-- [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use clap::Parser;
+// use cuid;
+
+/// Simple program to generate CUIDs
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Prefix of the CUID
+    #[arg(short, long)]
+    prefix: Option<String>,
+
+    #[arg(short, long)]
+    slug: bool,
+
+    /// Number of CUIDs to generate
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    // println!("{}", args.prefix.as_deref().unwrap_or_default());
+
+    // let prefix = if let Some(pre) = args.prefix.as_deref() {
+    //     pre.to_string()
+    // } else {
+    //     "".to_string()
+    // };
+
+    for _ in 0..args.count {
+        println!(
+            "{}{}",
+            args.prefix.as_deref().unwrap_or_default(),
+            cuid::cuid().unwrap()
+        )
+    }
 }
